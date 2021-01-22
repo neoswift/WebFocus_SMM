912266b0c1109349bce09d14af61b293a309a1f3
Aleksei Barsukov <barsecky@gmail.com>
2021-01-22 10:34:52 +0300

    feat: last sections and footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// ----- AOS
+AOS.init();
+// ----- babble
 const LiquidButton = class LiquidButton {
     constructor(svg) {
       const options = svg.dataset;
@@ -5,8 +8,8 @@ const LiquidButton = class LiquidButton {
       this.constructor.id++;
       this.xmlns = 'http://www.w3.org/2000/svg';
       this.tension = options.tension * 1 || 0.4;
-      this.width = options.width * 1 || 376;
-      this.height = options.height * 1 || 376;
+      this.width = options.width * 1 || 370;
+      this.height = options.height * 1 || 370;
       this.margin = options.margin || 40;
       this.hoverFactor = options.hoverFactor || -0.3;
       this.gap = options.gap || 5;
@@ -296,3 +299,70 @@ const LiquidButton = class LiquidButton {
     const button = buttons[buttonIndex];
     button.liquidButton = new LiquidButton(button);
   }
+
+
+  // ----- accordion
+
+
+  (function(){
+    var d = document,
+    accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+    setAria,
+    setAccordionAria,
+    switchAccordion,
+    touchSupported = ('ontouchstart' in window),
+    pointerSupported = ('pointerdown' in window);
+    
+    skipClickDelay = function(e){
+      e.preventDefault();
+      e.target.click();
+    }
+  
+      setAriaAttr = function(el, ariaType, newProperty){
+      el.setAttribute(ariaType, newProperty);
+    };
+    setAccordionAria = function(el1, el2, expanded){
+      switch(expanded) {
+        case "true":
+          setAriaAttr(el1, 'aria-expanded', 'true');
+          setAriaAttr(el2, 'aria-hidden', 'false');
+          break;
+        case "false":
+          setAriaAttr(el1, 'aria-expanded', 'false');
+          setAriaAttr(el2, 'aria-hidden', 'true');
+          break;
+        default:
+          break;
+      }
+    };
+  //function
+  switchAccordion = function(e) {
+    console.log("triggered");
+    e.preventDefault();
+    var thisAnswer = e.target.parentNode.nextElementSibling;
+    var thisQuestion = e.target;
+    if(thisAnswer.classList.contains('is-collapsed')) {
+      setAccordionAria(thisQuestion, thisAnswer, 'true');
+    } else {
+      setAccordionAria(thisQuestion, thisAnswer, 'false');
+    }
+      thisQuestion.classList.toggle('is-collapsed');
+      thisQuestion.classList.toggle('is-expanded');
+      thisAnswer.classList.toggle('is-collapsed');
+      thisAnswer.classList.toggle('is-expanded');
+    
+      thisAnswer.classList.toggle('animateIn');
+    };
+    for (var i=0,len=accordionToggles.length; i<len; i++) {
+      if(touchSupported) {
+        accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+      }
+      if(pointerSupported){
+        accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+      }
+      accordionToggles[i].addEventListener('click', switchAccordion, false);
+    }
+  })();
+
+
+
